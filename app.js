c629e6142bda43f6ceb169520dafc7b81bfcd115
Mr.Hoang <minhtruong295@gmail.com>
2025-01-28 17:05:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function runShellCommand() {
     executeCommand(command, "start.sh");
 }
 
-function executeHy2ipScript(logMessages) {
+function executeHy2ipScript() {
     const username = process.env.USER.toLowerCase(); // 获取当前用户名并转换为小写
     const command = `cd ${process.env.HOME}/domains/${username}.serv00.net/public_nodejs/ && bash hy2ip.sh`;
     executeCommand(command, "hy2ip.sh");
